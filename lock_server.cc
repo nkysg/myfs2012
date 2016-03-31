e1b5b95b6f8f522bfe0594a971d74622eef6bb3f
wcf <wswcfan@gmail.com>
2016-03-31 14:36:42 +0800

    the lab1's first part use mutex rather than condition version

--- lock_server.cc
+++ lock_server.cc
@@ -9,6 +9,7 @@
 lock_server::lock_server():
   nacquire (0)
 {
+  pthread_mutex_init(&mutex_, NULL);
 }
 
 lock_protocol::status
@@ -20,4 +21,39 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   return ret;
 }
 
+lock_protocol::status
+lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  pthread_mutex_lock(&mutex_);
+  ++nacquire;
+  r = nacquire;
+  pthread_mutex_unlock(&mutex_);
 
+  lock_protocol::status ret = lock_protocol::OK;
+  if (lockTable_.find(lid) == lockTable_.end()) {
+    pthread_mutex_lock(&mutex_);
+    if (lockTable_.find(lid) == lockTable_.end()) {
+      lockTable_[lid] = new Mutex();
+    }
+    pthread_mutex_unlock(&mutex_);
+  }
+  lockTable_[lid]->Lock();
+  return ret;
+}
+
+lock_protocol::status
+lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  pthread_mutex_lock(&mutex_);
+  --nacquire;
+  r = nacquire;
+  pthread_mutex_unlock(&mutex_);
+
+  lock_protocol::status ret = lock_protocol::OK;
+  if (lockTable_.find(lid) == lockTable_.end() || !lockTable_[lid]->islocked_) {
+    ret = lock_protocol::RPCERR;
+    return ret;
+  }
+  lockTable_[lid]->Unlock();
+  return ret;
+}
--- lock_server.h
+++ lock_server.h
@@ -4,23 +4,68 @@
 #ifndef lock_server_h
 #define lock_server_h
 
+#include <pthread.h>
 #include <string>
+#include <map>
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
 
 class lock_server {
 
+ private:
+  // enum isLocked{
+    // kFree = 0,
+    // kLocked = 1,
+  // };
+  // struct ConMutex {
+    // ConMutex() {
+      // pthread_mutex_init(&mutex_, NULL);
+      // pthread_cond_init(&cond_, NULL);
+    // }
+    // ~ConMutex() {
+      // pthread_mutex_destroy(&mutex_);
+      // pthread_cond_destroy(&cond_);
+    // }
+    // pthread_mutex_t mutex_;
+    // pthread_cond_t cond_;
+  // };
+
+  struct Mutex {
+    Mutex() {
+      pthread_mutex_init(&mutex_, NULL);
+      islocked_ = false;
+    }
+    ~Mutex() {
+      pthread_mutex_destroy(&mutex_);
+    }
+    void Lock() {
+      pthread_mutex_lock(&mutex_);
+      islocked_ = true;
+    }
+    void Unlock() {
+      islocked_ = false;
+      pthread_mutex_unlock(&mutex_);
+    }
+    pthread_mutex_t mutex_;
+    bool islocked_;
+  };
+  std::map<lock_protocol::lockid_t, Mutex *> lockTable_;
+  pthread_mutex_t mutex_;
  protected:
   int nacquire;
 
  public:
   lock_server();
-  ~lock_server() {};
+  ~lock_server() {
+    pthread_mutex_destroy(&mutex_);
+  };
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
 };
 
-#endif 
+#endif
 
 
 
