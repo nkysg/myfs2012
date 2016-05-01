3d861fe106965a40d0d1a12f94c5621344a2ef1c
wcf <wswcfan@gmail.com>
2016-05-02 04:22:03 +0800

    modify yfs_client's lc to lock_client_cache

--- yfs_client.cc
+++ yfs_client.cc
@@ -31,8 +31,8 @@ random_number_seed()
 yfs_client::yfs_client(std::string extent_dst, std::string lock_dst)
 {
   ec = new extent_client(extent_dst);
-  lc = new lock_client(lock_dst);
-  // lc = new lock_client_cache(lock_dst);
+  // lc = new lock_client(lock_dst);
+  lc = new lock_client_cache(lock_dst);
 
   // init the seed for random
   srandom(random_number_seed());
